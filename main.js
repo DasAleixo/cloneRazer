02eea4b8b61395c1e4e3fd6b8baca63d5882833c
DasAleixo <contato.victorzmr@gmail.com>
2023-05-13 15:09:06 -0300

    new

---
+++ main.js
@@ -0,0 +1,34 @@
+window.addEventListener("DOMContentLoaded", (event) => {
+    //mudar cor texto
+    const test = document.getElementById("test");
+    function newcolor(color, evt) {
+        for (i=0; i <= 10; i+=1) {
+            var imgid = `img${i}`;
+            if (evt.target.id == imgid) {
+                var txt = document.getElementById(`txt${i}`);
+                txt.style.color = color
+            }
+        }
+    }
+    test.addEventListener('mouseover', (event) => {newcolor('rgb(67, 255, 98)', event)})
+    test.addEventListener('mouseout', (event) => {newcolor('white', event)})
+    //--------------------------------------------------------------------------------------
+
+    //menu hamburguer
+    const menu = document.getElementById("menu");
+    const sidebar = document.getElementById("sidebar")
+
+    menu.addEventListener('click', function() {
+        if (sidebar.style.display == 'block') {
+            sidebar.style.visibility = 'hidden';
+            sidebar.style.display = 'none';
+        }
+        else {
+            sidebar.style.translate = 25
+            sidebar.style.display = 'block'
+            sidebar.style.visibility = 'visible';
+        }
+    });
+    
+})
+
